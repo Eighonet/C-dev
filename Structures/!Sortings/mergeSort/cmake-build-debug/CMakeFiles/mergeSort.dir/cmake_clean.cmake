143336f6285aa1a4766796cddb3edd4b7cee0708
Vadim Porvatov <eighonet@gmail.com>
2020-01-03 16:07:37 +0400

    MerheSort cont

---
+++ Structures/!Sortings/mergeSort/cmake-build-debug/CMakeFiles/mergeSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mergeSort.dir/main.cpp.obj"
+  "mergeSort.exe"
+  "mergeSort.exe.manifest"
+  "mergeSort.lib"
+  "mergeSort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mergeSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
